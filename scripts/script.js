f82a6e158801f5e464fea7e94caaa6e35c21956e
jack <jack@example.com>
2025-03-30 15:02:13 -0300

    inital commit

---
+++ scripts/script.js
@@ -0,0 +1,126 @@
+let highToLow;
+
+getDivision('all')
+
+
+function getDivision(division) {
+
+
+
+
+
+
+
+
+
+    fetch('https://falling-frog-ec91.jmi06.workers.dev')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Response was not ok')
+
+            }
+            return response.json();
+        })
+
+        .then(data => {
+
+            generate_table(division, data)
+
+        })
+
+        .catch(error => {
+            console.error('Error: ', error)
+        })
+
+
+
+       
+}
+
+
+
+
+
+function generate_table(division, data) {
+    // Race data is "All" if its just all driver standings
+
+
+
+
+
+        orderData = data[division]
+        
+        const table = document.getElementById('data-table')
+
+        const tableHead = document.getElementById("table-head");
+        const tableBody = document.getElementById("table-body");
+
+        tableHead.innerText = ""
+
+        const headers = ['Pos', 'Team', 'Elo', 'Games', 'Record']
+        const header_stat = ["team_name", "elo", 'games', "record"]
+
+        // Do Playoff points and position separate
+
+
+
+
+        headers.forEach(header => {
+            const th = document.createElement("th");
+            th.textContent = header;
+
+
+            tableHead.appendChild(th);
+        });
+
+        let new_stat;
+        tableBody.innerText = ""
+        Object.entries(orderData).forEach((team, index) => {
+            console.log(team)
+            const tr = document.createElement("tr");
+
+            const positionTd = document.createElement("td");
+            positionTd.textContent = index + 1; // Position starts from 1, not 0
+            tr.appendChild(positionTd);
+
+            header_stat.forEach(stat => {
+                if(stat == 'team_name'){
+                    new_stat = team[0]
+                }
+                if(stat == 'elo'){
+                    new_stat = team[1]['elo']
+                }
+
+                if (stat == "games") {
+                    new_stat = team[1]['games'].length
+
+
+                } 
+                if (stat == "record"){
+                    if(team[1]['record']){
+
+                        new_stat = team[1]['record'].split('/')
+                        new_stat = new_stat[0] / (new_stat[0] + new_stat[1])
+                    } else{
+                        new_stat = '--'
+                    }
+                } 
+               
+                const td = document.createElement("td");
+                td.textContent = new_stat || ''; // Handle undefined properties
+                tr.appendChild(td);
+            });
+
+
+            tableBody.appendChild(tr);
+
+
+        });
+
+
+
+
+    } 
+
+
+
