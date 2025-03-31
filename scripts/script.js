2cd4f34aed5492b66333b061f13111517b63136d
jack <jack@example.com>
2025-03-31 08:48:02 -0300

    fixed national league stats

--- scripts/script.js
+++ scripts/script.js
@@ -68,7 +68,14 @@ function generate_table(division, data) {
         headers.forEach(header => {
             const th = document.createElement("th");
             th.textContent = header;
+            th.onclick = () => {
+                if (th.textContent != "Pos" || th.textContent != "Team"){
 
+                // chosen_year = year; // Update chosen_year
+                highToLow = !highToLow   
+                sortTable(highToLow, header, data, division);
+                }
+            };
 
             tableHead.appendChild(th);
         });
@@ -84,6 +91,9 @@ function generate_table(division, data) {
             tr.appendChild(positionTd);
 
             header_stat.forEach(stat => {
+
+
+                
                 if(stat == 'team_name'){
                     new_stat = team[0]
                 }
@@ -132,3 +142,83 @@ function generate_table(division, data) {
 
 
 
+
+
+
+
+
+
+
+function sortTable(highToLow, column, data, division){
+
+
+    if(highToLow == true){
+
+        
+
+            if(column == "Elo"){
+                new_column = "elo"
+            } 
+    
+            if (column == "Games"){
+                new_column ="race_num"
+            } 
+            if (column == "Record"){
+                new_column ="games"
+            } 
+
+            
+            const dataArray = Object.entries(data[division]);
+
+            let sorted_teams = dataArray.sort((a, b) => a[new_column] - b[new_column]);
+            console.log(sorted_teams)
+            const sortedData = Object.fromEntries(sorted_teams);
+            console.log(sortedData)
+            generate_table(sortedData, "All")
+
+
+
+
+        
+
+
+
+    } else if(highToLow == false){
+
+        
+
+            if(column == "Elo"){
+                new_column = "elo"
+            } 
+            if (column == "Name"){
+                new_column ="name"
+            } 
+            if (column == "Races"){
+                new_column ="race_num"
+            } 
+            if (column == "Playoff Points"){
+                new_column ="playoff_points"
+            } 
+            if(column == 'NASCAR Position'){
+                new_column = 'position'
+            }
+            const dataArray = Object.values(data);
+            
+            let sorted_teams = dataArray.sort((a, b) => a[new_column] - b[new_column]);
+            sorted_teams = sorted_teams.reverse()
+            const sortedData = Object.fromEntries(sorted_teams);
+
+            generate_table(sortedData, "All")
+
+
+
+
+         
+
+
+
+
+
+    }
+
+}
